a33145035b209792451bb9967f5c2ee30f1e7fc0
Xavi Fortes <53091080+XaviFortes@users.noreply.github.com>
2021-02-02 09:51:44 +0100

    Update leap-year.c
    Added the code

--- leap-year.c
+++ leap-year.c
@@ -1,1 +1,32 @@
+/*
+ ============================================================================
+ Name        : Test00.c
+ Author      : Xavi Fortes
+ Version     :
+ Copyright   : Xavi Fortes 2021
+ Description : Leap Year
+ ============================================================================
+ */
 
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+	int year;
+	puts("Put the year you want to know if it's leap.");
+	fflush(stdout);
+	scanf("%d", &year);
+	if (year % 400 == 0) {
+		printf("%d is a leap year.", year);
+	}
+	else if(year % 100 == 0) {
+		printf("%d isn't a leap year.", year);
+	}
+	else if(year % 4 == 0) {
+		printf("%d is a leap year.", year);
+	}
+	else {
+		printf("%d isn't a leap year.", year);
+	}
+	return 0;
+}

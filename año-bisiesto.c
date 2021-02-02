7d2d79e1c36234ac8d7f4b03e666486b7b69d56a
Xavi Fortes <53091080+XaviFortes@users.noreply.github.com>
2021-02-02 09:50:00 +0100

    Added the code

--- año-bisiesto.c
+++ año-bisiesto.c
@@ -1,1 +1,32 @@
+/*
+ ============================================================================
+ Name        : Test00.c
+ Author      : Xavi Fortes
+ Version     :
+ Copyright   : Xavi Fortes 2021
+ Description : Año Bisiesto en C
+ ============================================================================
+ */
 
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+	int year;
+	puts("Pon el año que quieres saber si es bisiesto.");
+	fflush(stdout);
+	scanf("%d", &year);
+	if (year % 400 == 0) {
+		printf("%d es un año bisiesto.", year);
+	}
+	else if(year % 100 == 0) {
+		printf("%d no es un año bisiesto.", year);
+	}
+	else if(year % 4 == 0) {
+		printf("%d es un año bisiesto.", year);
+	}
+	else {
+		printf("%d no es un año bisiesto.", year);
+	}
+	return 0;
+}
